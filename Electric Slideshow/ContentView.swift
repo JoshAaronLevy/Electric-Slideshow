88ed374829c64536357379f70f51a3a34561b85a
JoshAaronLevy <joshaaronlevy@gmail.com>
2025-11-20 10:25:40 -0700

    Initial Commit

---
+++ Electric Slideshow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Electric Slideshow
+//
+//  Created by Josh Levy on 11/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Electric Slideshow/Electric_SlideshowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Electric_SlideshowApp.swift
+//  Electric Slideshow
+//
+//  Created by Josh Levy on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Electric_SlideshowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
